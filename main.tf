f7cf49f999ff1b4656fc6f35b6173f1193173834
Nick Fagerlund <nick.fagerlund@gmail.com>
2023-01-10 11:51:05 -0800

    Ok, custom dynamic resource instead, this seems to plan just fine!

--- main.tf
+++ main.tf
@@ -11,18 +11,15 @@ provider "tfcoremock" {
   # Configuration options
 }
 
-resource "tfcoremock_complex_resource" "rancher2_cluster" {
+resource "tfcoremock_dynamic_resource" "rancher2_cluster" {
     id = "the_boi"
 
     string = "some description, whatever"
-    list_block { # rke_config
-        list_block { # ingress
-            string = "hey"
-        }
-        list_block { # services
-            list_block { # etcd
-                list_block { # backup_config
-                    list_block { # s3_backup_config
+    rke_config { # rke_config
+        services { # services
+            etcd { # etcd
+                backup_config { # backup_config
+                    s3_backup_config { # s3_backup_config
                         string = "stuff, idk, region"
                     }
                 }
